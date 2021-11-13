0aa274558e194493b779a9de6de3c767a39a78d1
dev@suomikulta <dev@suomikulta.fi>
2021-11-13 21:12:26 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,29 @@
+module.exports = {
+	extends: [
+		// Reasonable config for CSS and SCSS
+		"stylelint-config-sass-guidelines"
+	],
+
+	rules: {
+		// Rule overrides for stylelint-config-sass-guidelines
+		"color-hex-length": "long",
+		indentation: "tab",
+		"max-nesting-depth": null,
+		"order/properties-alphabetical-order": null,
+		"selector-no-qualifying-type": null,
+
+		// Additional rules
+		"declaration-no-important": true,
+		"font-family-name-quotes": "always-unless-keyword",
+		"font-weight-notation": "numeric",
+		"function-url-no-scheme-relative": true,
+		"selector-attribute-quotes": "always",
+		"string-quotes": "double",
+	},
+	plugins: [
+		"stylelint-declaration-strict-value",
+		"stylelint-scss",
+		"stylelint-order",
+
+	]
+};
